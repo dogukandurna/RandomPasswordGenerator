4351f45fd91ac5eb870ad9f481dfeef6efff5a69
dogukandurna <fdgodreis@gmail.com>
2022-09-23 13:51:48 +0300

    Random Password Creater C# Files

---
+++ RandomPasswordCreater/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace RandomPasswordCreater
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(39, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 106);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ŞİFRE OLUŞTURUCU";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(204, 267);
+            this.textBox1.MinimumSize = new System.Drawing.Size(172, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(172, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(204, 144);
+            this.button1.MinimumSize = new System.Drawing.Size(172, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OLUŞTUR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(39, 226);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 28);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Sayılar";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox2.Location = new System.Drawing.Point(39, 267);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(103, 28);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "İşaretler";
+            this.checkBox2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox2.Location = new System.Drawing.Point(39, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(100, 24);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Uzunluk";
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox4.Location = new System.Drawing.Point(39, 185);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(91, 28);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Harfler";
+            this.checkBox4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(204, 185);
+            this.button2.MinimumSize = new System.Drawing.Size(172, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "KOPYALA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(204, 226);
+            this.button3.MinimumSize = new System.Drawing.Size(172, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 35);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "RASTGELE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 331);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(415, 370);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ŞİFRE OLUŞTURUCU";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+    }
+}
+
---
+++ RandomPasswordCreater/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomPasswordCreater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox2.Text == "" || textBox2.Text == "Uzunluk")
+            {
+                errorProvider2.SetError(textBox2,"Uzunluk Boş Geçilemez");
+                errorProvider1.BlinkRate = 0;
+            }
+            else
+            {
+                errorProvider2.Clear();
+                int a = Convert.ToInt32(textBox2.Text);
+                int uzunluk = a;
+                StringBuilder result = new StringBuilder();
+                Random rast = new Random();
+                textBox1.MaxLength = 25;
+
+                for (int i = 0; i < uzunluk; i++)
+                {
+                    const string Harfler = "abcçdefgğhiıjklmnoöprsştuüvyzABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ";
+                    const string İfadeler = "!>'£^#+$%&/()=?*-_|";
+                    const string İfaHarf = "!>'£^#+$%&/()=?*-_|!>'£^#+=?*-_abcçdefgğhiıjklmnoöprsştuüvyzABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ";
+                    const string Sayİfa = "0123456789!>'£^#+$%&/()=?*-_|";
+                    string karışık = "0123456789012345678901234567890123456789012345abcçdefgğhiıjklmnoöprsştuüvyzabcçdefgğhiıjklmnoöprsştuüvyzABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZabcçdefgğhi";
+                    string hepsi = "!>'£^#+$%&/()=?*-_|0123456789!>'£^#+$%&/()=?*-_|abcçdefgğhiıjklmnoöprsştuüvyzABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ";
+                    // string[] SayHarf = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a","b","c","ç","d","e","f","g","ğ","hiıjklmnoöprsştuüvyzABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ" }; */
+                    if (checkBox4.Checked && !checkBox1.Checked && !checkBox2.Checked)
+                    {
+                        result.Append(Harfler[rast.Next(Harfler.Length)]);
+                    }
+                    else if (!checkBox4.Checked && checkBox1.Checked && !checkBox2.Checked)
+                    {
+                        // result.Append(Sayılar[rast.Next(Sayılar.Length)]);
+                        int şifre = rast.Next(48, 57);
+                        char karakter = Convert.ToChar(şifre);
+                        result.Append(karakter);
+                    }
+                    else if (!checkBox4.Checked && !checkBox1.Checked && checkBox2.Checked)
+                    {
+                        result.Append(İfadeler[rast.Next(İfadeler.Length)]);
+                    }
+                    else if (checkBox1.Checked && checkBox4.Checked && !checkBox2.Checked)
+                    {
+                        result.Append(karışık[rast.Next(karışık.Length)]);
+                    }
+                    else if (checkBox4.Checked && !checkBox1.Checked && checkBox2.Checked)
+                    {
+                        result.Append(İfaHarf[rast.Next(İfaHarf.Length)]);
+                    }
+                    else if (!checkBox4.Checked && checkBox1.Checked & checkBox2.Checked)
+                    {
+                        result.Append(Sayİfa[rast.Next(Sayİfa.Length)]);
+                    }
+                    else if (checkBox4.Checked && checkBox1.Checked && checkBox2.Checked)
+                    {
+                        result.Append(hepsi[rast.Next(hepsi.Length)]);
+                    }
+
+                    errorProvider1.Clear();
+
+                    /* int şifre = rast.Next(32, 127);
+                    char karakter = Convert.ToChar(şifre);
+                    result.Append(karakter); */
+                }
+
+                textBox1.Text = result.ToString();
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                errorProvider1.SetError(textBox1,"Şifre Alanı boş");
+                errorProvider1.BlinkRate = 0;
+            }
+            else
+            {
+                errorProvider1.Clear();
+                Clipboard.SetText(textBox1.Text);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "" || textBox2.Text == "Uzunluk")
+            {
+                errorProvider2.SetError(textBox2, "Uzunluk Boş Geçilemez");
+                errorProvider1.BlinkRate = 0;
+            }
+            else
+            {
+                errorProvider2.Clear();
+                int a = Convert.ToInt32(textBox2.Text);
+                int uzunluk = a;
+                StringBuilder result = new StringBuilder();
+                Random rast = new Random();
+
+                for (int i = 0; i < uzunluk; i++)
+                {
+                    int şifre = rast.Next(32, 127);
+                    char karakter = Convert.ToChar(şifre);
+                    result.Append(karakter);
+                }
+                textBox1.Text = result.ToString();
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }
+}
